378783884c1506fce447f3b016f257cce98a335c
Leonid2377 <leonid2377@mail.ru>
2022-03-24 21:55:21 +0300

    v1.4

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -26,12 +26,11 @@ class Post(models.Model):
     )  # поле для картинки
     group = models.ForeignKey(
         Group, on_delete=models.CASCADE,
-        related_name='posts', blank=True, null=True
+        related_name="posts", blank=True, null=True
     )
 
     def __str__(self):
         return self.text
-
 
 class Comment(models.Model):
     author = models.ForeignKey(
